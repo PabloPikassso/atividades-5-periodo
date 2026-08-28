4069b7dcf6c4e570eb799716cd8bf35624e120d8
PabloPikasso <pabloantoning@gmail.com>
2026-08-28 19:47:43 -0300

    tarefa 05 calculadora em js

---
+++ atividade05/calculadora.js
@@ -0,0 +1,9 @@
+const valorConta = 100
+
+const percentualGorjeta =  15
+
+const gorjetaFinal = valorConta * (percentualGorjeta/100)
+
+const valorFinal = gorjetaFinal + valorConta
+
+console.log("O valor da conta foi de: "+ valorConta+", % da gorjeta sao 15% entao o valor e: "+ gorjetaFinal +"\n O total a pagar sera "+valorFinal)
